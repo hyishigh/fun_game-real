4fd4926c765df7f762176c43d300a01732c8bba3
hyishigh <99009198+hyishigh@users.noreply.github.com>
2024-06-19 15:03:40 +0800

    Create ip.php

---
+++ ip.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hy's little game</title>
+</head>
+<body>
+    <header>
+        <h1>Yo nice ip address bro</h1>
+    </header>
+    <nav>
+    </nav>
+    <section>
+        <p>hola</p>
+        <?php
+            $ip = $_SERVER['REMOTE_ADDR'];
+            file_put_contents('logs.txt', $ip, FILE_APPEND);
+            $details = json_decode(file_get_contents("http://ipinfo.io/{$ip}/json"));
+        ?>
+    </section>
+    <footer>
+        <p>&copy; Hy's website</p>
+    </footer>
+</body>
+</html>
